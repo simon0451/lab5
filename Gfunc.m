e7a505b532a9ff21ccd4a07f592874003c5fc6db
Simon <sapopecki@gmail.com>
2017-03-26 21:18:23 -0400

    first deliverable done, second nearly done

--- Gfunc.m
+++ Gfunc.m
@@ -1,8 +1,11 @@
-function [stepy] = Gfunc(wn,zeta,t)
+function [stepy] = Gfunc(rawDataArray,dataArray,wn,zeta)
 
-num=2;%K value
+t = dataArray(:,1);
+voltages = rawDataArray(:,2);
+initialsig = voltages(1);
+finalsig = voltages(end);
+num=finalsig-initialsig;%K value
 den = [1/wn^2 2*zeta/wn 1];
-initialsig=3;
 sys=tf(num,den);
 stepy = step(sys+initialsig,t);
 end
