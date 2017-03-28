f773793a73a8710073f30b944fbc7e4b716d73c6
Simon <sapopecki@gmail.com>
2017-03-28 14:03:37 -0400

    modifications on some functions for clarity

--- Gfunc.m
+++ Gfunc.m
@@ -1,4 +1,5 @@
 function [stepy] = Gfunc(rawDataArray,dataArray,wn,zeta)
+%creates a simulated waveform for a step function given the data the natural frequency, and the damping ratio
 
 t = dataArray(:,1);
 voltages = rawDataArray(:,2);
@@ -8,4 +9,5 @@ num=finalsig-initialsig;%K value
 den = [1/wn^2 2*zeta/wn 1];
 sys=tf(num,den);
 stepy = step(sys+initialsig,t);
+
 end
